4b4accb893034b1db90aff449459663333fb8897
Hariprasad Satishkumar Korade <68178339+korade-hariprasad@users.noreply.github.com>
2024-07-01 15:23:25 +0530

    added list item for showing scrollable list fetched from the api.

--- app/src/main/java/sumagoscope/madipt/b3volleydemo/MainActivity.java
+++ app/src/main/java/sumagoscope/madipt/b3volleydemo/MainActivity.java
@@ -24,76 +24,41 @@ import org.json.JSONObject;
 public class MainActivity extends AppCompatActivity {
 
     TextView tvResponse;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        tvResponse=findViewById(R.id.tvResponse);
+        tvResponse = findViewById(R.id.tvResponse);
         getDataFromServer();
     }
 
     private void getDataFromServer() {
-
-        RequestQueue requestQueue= Volley.newRequestQueue(this);
-        String url="https://dummyjson.com/products?select=title,price,thumbnail,category";
-
-        StringRequest stringRequest=new StringRequest(Request.Method.GET, url,
-                new Response.Listener<String>() {
-                    @Override
-                    public void onResponse(String response) {
-                        Log.d("mytag",response);
-
-                        tvResponse.setText(response);
-
-                        try {
-                            JSONObject mainObject=new JSONObject(response);
-                            JSONArray jsonArray=mainObject.getJSONArray("products");
-
-                            for(int i=0;i<jsonArray.length();i++)
-                            {
-
-                                JSONObject jsonObject=jsonArray.getJSONObject(i);
-                                Log.d("mytag",jsonObject.getString("title"));
-                                Log.d("mytag",""+jsonObject.getDouble("price"));
-
-                            }
-
-                        } catch (JSONException e) {
-                            throw new RuntimeException(e);
-                        }
-
-                    }
-                }, new Response.ErrorListener() {
-            @Override
-            public void onErrorResponse(VolleyError error) {
-                Log.d("mytag","onErrorResponse => "+error.getMessage());
-            }
-        }
-        );
-        requestQueue.add(stringRequest);
-    }
-
-    public  void getListFromServer(){
-
-        RequestQueue requestQueue=Volley.newRequestQueue(MainActivity.this);
-        String url="https://dummyjson.com/products?select=title,price,thumbnail,category";
-
-        StringRequest stringRequest=new StringRequest(Request.Method.GET, url, new Response.Listener<String>() {
+        RequestQueue requestQueue = Volley.newRequestQueue(this);
+        String url = "https://dummyjson.com/products?select=title,price,thumbnail,category";
+        StringRequest stringRequest = new StringRequest(Request.Method.GET, url, new Response.Listener<String>() {
             @Override
             public void onResponse(String response) {
-
-                Log.d("mytag",response);
-
+                Log.d("mytag", response);
+                tvResponse.setText(response);
+                try {
+                    JSONObject mainObject = new JSONObject(response);
+                    JSONArray jsonArray = mainObject.getJSONArray("products");
+                    for (int i = 0; i < jsonArray.length(); i++) {
+                        JSONObject jsonObject = jsonArray.getJSONObject(i);
+                        Log.d("mytag", jsonObject.getString("title"));
+                        Log.d("mytag", "" + jsonObject.getDouble("price"));
+                    }
+                } catch (JSONException e) {
+                    throw new RuntimeException(e);
+                }
             }
         }, new Response.ErrorListener() {
             @Override
             public void onErrorResponse(VolleyError error) {
-
-                Log.d("mytag",error.getMessage());
-
+                Log.d("mytag", "onErrorResponse => " + error.getMessage());
             }
         });
-
         requestQueue.add(stringRequest);
     }
 }
